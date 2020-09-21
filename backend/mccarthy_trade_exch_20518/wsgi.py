11a5e566ecac1074befff5397891b871d2b40ad4
Crowdbotics <team@crowdbotics.com>
2020-09-21 19:15:40 +0000

    Update models

--- backend/mccarthy_trade_exch_20518/wsgi.py
+++ backend/mccarthy_trade_exch_20518/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mccarthy_trade_exch_20518.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mccarthy_trade_exch_20518.settings")
 
 application = get_wsgi_application()
